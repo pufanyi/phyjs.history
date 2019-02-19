cec8a014c176a78fff74eaf42e2867be0d8463a7
pufanyi <pufanyi.gmail.com>
2019-02-19 19:01:01 +0800

    add-mkmap

---
+++ mkmap/mkmap1.cpp
@@ -0,0 +1,35 @@
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ofstream fout("map.out");
+	for(int i = 1; i <= 100; ++i)
+	{
+		fout << "[";
+		for(int j = 1; j <= 10; ++j)
+			fout << "\'air\', ";
+		for(int j = 11; j <= 50; ++j)
+			fout << ((rand() % 10) ? "\'air\', " : "\'block\', ");
+		for(int j = 50; j < 60; ++j)
+			fout << "\'air\', ";
+		fout << "\'air\'],";
+		fout << '\n';
+	}
+	fout << "[";
+	for(int i = 1; i < 60; ++i)
+		fout << "\'trap\', ";
+	fout << "\'trap\'],";
+	fout << '\n';
+	fout << "[";
+	for(int i = 1; i < 60; ++i)
+		fout << "\'block\', ";
+	fout << "\'block\'],";
+	fout << '\n';
+	fout.close();
+	return 0;
+}
